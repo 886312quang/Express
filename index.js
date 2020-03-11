b10b762e69117e45271e2759dc138e84e03b3d72
QuangPham <886312quangpham@gmail.com>
2020-03-11 23:56:55 +0700

    bai9

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
 var express = require('express');
-var app = express();
+const bodyParser = require('body-parser');
+
+
+var userRoute = require('./routes/user.route');
+
+// Set some defaults (required if your JSON file is empty)
+
 var port = 5500;
-var users = [
-    { id: 1, name: 'MQ' },
-    { id: 2, name: 'MQ2' }
-]
+
+var app = express();
 app.set('view engine', 'pug');
 app.set('views', './view');
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.get('/', function (req, res) {
     res.render('index', {
         name: 'MQ'
     });
 });
-app.get('/users', function (req, res) {
-    res.render('users/index', {
-        users: users
-    });
-});
-app.get('/users/search', function (req, res) {
-    var q = req.query.q;
-    var matchedUser = users.filter(function (users) {
-        return users.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
-    });
-    res.render('users/index', {
-        users: matchedUser
-    })
-});
-app.get('/users/create', function (req, res) {
-    res.render('users/create');
-});
-app.post('/users/create', function(req,res){
-    users.push(req.body);
-    res.redirect('/users');
-});
+app.use('/users', userRoute);
+
 app.listen(port, function () {
-    console.log('Sever listening on port' + port)
+    console.log('Server listening on port' + port);
 });
+
+
