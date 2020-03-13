032a1592cb5236312d6a0dd65e56106fc495353d
QuangPham <886312quangpham@gmail.com>
2020-03-13 23:45:24 +0700

    done pagination

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -5,25 +5,30 @@ module.exports.index = function (req, res) {
     var perPage = 8; //x
     var start = (page - 1) * perPage;
     var end = page * perPage;
-    //drop
+    var next=page+1;
+    var previous=page-1;
+ 
     var quatityPage = Math.ceil(db.get('product')
         .size()
         .value() / perPage);
-    
     var sizePage = db.get('dem')
                     .size()
                     .value();
+    if(next>sizePage){
+        next=page;
+    }
     if(sizePage<quatityPage){
     for (var i = sizePage+1; i <= quatityPage; i++) {
         db.get('dem')
             .push({ id:""+i+"", title: 'lowdb is awesome'})
             .write()
+        }
     }
-}
     var drop = (page - 1) * perPage;
     res.render('product/index', {
         product: db.get('product').drop(drop).take(perPage).value(),
-        dem: db.get('dem').value()
-
+        dem: db.get('dem').value(),
+        next: next,
+        previous: previous
     });
 };
